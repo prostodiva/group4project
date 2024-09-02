baa91eb3f7b2a7778aaa9d3785eb9639cb77398b
prostodiva <rkattsyna@mail.ru>
2024-09-02 13:29:32 -0700

    programs->sections have been created

--- programs/script.js
+++ programs/script.js
@@ -8,14 +8,14 @@ document.addEventListener("DOMContentLoaded", function() {
             //smooth color change
             event.target.style.transition="all 0.5s";
             event.target.style['text-decoration']='underline';
-        });
+        }); 
     //color reset
     menuItemElement.addEventListener("mouseout", 
         (event)  => {
             event.target.style.color ="";
             event.target.style.transition="all 0.3s";
             event.target.style['text-decoration']='';
-        });   
+        });
     //support
     const supportElement = document.getElementById("active2");
     supportElement.addEventListener("mouseover", 
