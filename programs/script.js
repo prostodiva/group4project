97717b65d0c8bb8876b9f5df0926b4cfbdf5291c
prostodiva <rkattsyna@mail.ru>
2024-09-02 11:56:34 -0700

    smooth color change transition added

--- programs/script.js
+++ programs/script.js
@@ -5,12 +5,15 @@ document.addEventListener("DOMContentLoaded", function() {
     menuItemElement.addEventListener("mouseover", 
         (event) => {
             event.target.style.color= "#91d511";
+            //smooth color change
+            event.target.style.transition="all 0.5s";
             event.target.style['text-decoration']='underline';
         });
     //color reset
     menuItemElement.addEventListener("mouseout", 
         (event)  => {
             event.target.style.color ="";
+            event.target.style.transition="all 0.3s";
             event.target.style['text-decoration']='';
         });   
     //support
@@ -18,12 +21,14 @@ document.addEventListener("DOMContentLoaded", function() {
     supportElement.addEventListener("mouseover", 
         (event) => {
             event.target.style.color="black";
+            event.target.style.transition="all 0.5s";
             event.target.style['text-decoration']='underline';
         });
     //reset
     supportElement.addEventListener("mouseout", 
         (event) => {
             event.target.style.color="#91d511";
+            event.target.style.transition="all 0.3s";
             event.target.style['text-decoration']='';
         });
     });
